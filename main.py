c7b792f667a2279fb4facd437c0e544e4c4f98da
mbmbm <mbmbmbmb.32145@gmail.com>
2024-02-24 15:03:58 +0200

    1.2

--- main.py
+++ main.py
@@ -5,25 +5,23 @@ from bs4 import BeautifulSoup
 session = requests.Session()
 header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
 
-data = input("Input url categories ")
+data = "https://rozetka.com.ua/ua/shurupoverty-i-elektrootvertki/c152499/"
 for i in range(1, 10):
     print(f"PAGE -> {i}")
-    url = f"{data}p-{i}/"
+    url = f"{data}page={i}/"
     response = session.get(url, headers=header)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "lxml")
-        all_products = soup.find('div', class_="catalog-grid ng-star-inserted")
+        all_products = soup.find('ul', class_="catalog-grid ng-star-inserted")
         products = all_products.find_all('div', class_="goods-tile__content")
-        discount = all_products.find_all('div', class_="goods-tile__price-currency currency")
-
         for j in range(len(products)):
             try:
-                title_ = products[j].find("a", class_="catalog-grid ng-star-inserted").text
-                price = products[j].find("div", class_="goods-tile__content").text
-                discount = products[j].find("div", class_="goods-tile__price-currency currency").text
+                title_ = products[j].find("span", class_="goods-tile__title").text
+                price = products[j].find("div", class_="goods-tile__price-value").text
                 with open("products.txt", "a", encoding="UTF-8") as file:
-                    file.write(f"{title_}  {price}  {discount}\n")
-                print(title_, price, discount)
+                    file.write(f"{title_}  {price}\n")
+                print(title_, price)
             except:
+                title_ = products[j].find("span", class_="goods-tile__title").text
                 print(f"{title_} - знижки немає")
